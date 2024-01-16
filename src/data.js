f9ca8f8e281e149f4c9903f6f6fa17ab625fb67b
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2024-01-16 12:07:55 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -5,8 +5,8 @@ function chillHop() {
 
         {
             name:"Return of the Great White Hope",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
             audio: "https://media.transistor.fm/7c0b9448/3bb9c141.mp3?src=player",
@@ -14,8 +14,8 @@ function chillHop() {
          },
         {   
             name:"Adam v. Eve",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://media.transistor.fm/46172d29/287bfa27.mp3?src=player",
@@ -24,8 +24,8 @@ function chillHop() {
         {
             
             name:"The Russians are Coming",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://cdn.transistor.fm/file/transistor/m/shows/20235/2948204e1082b5fb3bdeecb8e1097dfb.mp3",
@@ -33,8 +33,8 @@ function chillHop() {
         },
         {
             name:"Man is a Free Agent",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://cdn.transistor.fm/file/transistor/m/shows/20235/5b3e46c6c0265656987ed55dfdb46b09.mp3",
@@ -42,8 +42,8 @@ function chillHop() {
         },
         {
             name:"Martes 28 de febrero",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#008781", "#cfc08c"],
             audio: "https://www.audioescuelasabatica.com/wp-content/uploads/2023/01/LECCION-11-MARTES.mp3?_=4",
@@ -51,8 +51,8 @@ function chillHop() {
         },
         {
             name:"Fight of the Century",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#845648","#FDFBDF"],
             audio: "https://cdn.transistor.fm/file/transistor/m/shows/20235/cdb0e3ccb38ba5c5e32e2d24e49def8e.mp3",
@@ -60,8 +60,8 @@ function chillHop() {
         },
         {
             name:"USA v. All Y’All",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#578CCE", "#E9D6D0"],
             audio: "https://cdn.transistor.fm/file/transistor/m/shows/20235/b9958a44682091e716e3d7696fb9c5a1.mp3",
@@ -69,8 +69,8 @@ function chillHop() {
         },
         {
             name:"Civil War on the Racetrack",
-            artist: "Sports News",
-            cover: "https://cdn-icons-png.flaticon.com/512/3909/3909383.png",
+            artist: "Kansas City Sports News",
+            cover: "https://img.freepik.com/fotos-premium/afiche-papel-tapiz-tema-futbol-americano-super-bowl-jugador-estadio-casco-futbol-americano_285885-2161.jpg",
             id: uuidv4(),
             color: ["#7BC8DD", "#D64A59"],
             audio: "https://cdn.transistor.fm/file/transistor/m/shows/20235/dfae447e6100b93988cc3dfca98da835.mp3",
