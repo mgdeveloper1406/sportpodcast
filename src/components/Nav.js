c263c9c36bbd849264a6289f0d4ca3b4a6e93618
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2024-01-16 12:10:21 -0300

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ const Nav = ({ setLibraryStatus, libraryStatus }) => {
   
     return (
       <nav>
-        <h1>Sports News</h1>
+        <h1>Kansas City Sports News</h1>
         <button
           className={libraryStatus ? "library-active" : ""}
           onClick={openLibraryHandler}
